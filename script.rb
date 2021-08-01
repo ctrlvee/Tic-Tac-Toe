378c27d93df09551b1d8a88afb3077a5ac26eaa0
ctrlVee <me123opportunity@gmail.com>
2021-07-31 21:02:04 -0400

    Developing Player class -- assigns X/O

--- script.rb
+++ script.rb
@@ -1,7 +1,16 @@
 require 'pry-byebug'
 
 class Player
+  attr_reader :player_marker
+  @@markers = ["X","O"].shuffle
+  @@player_count = 1
 
+  def initialize
+    @player_name = "Player #{@@player_count}"
+    @player_marker = @@markers.pop 
+    @@player_count += 1
+  end
+  
 end
 
 class Board
@@ -10,6 +19,10 @@ class Board
     @board_arr = Array.new(3,Array.new(3,nil))
   end
 end
+
 binding.pry
 board = Board.new
+p board
+p1 = Player.new
+p2 = Player.new
 p board
