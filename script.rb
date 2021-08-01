273503fc9c480fdd994ca233f1e7eabeb7a4664b
ctrlVee <me123opportunity@gmail.com>
2021-07-31 20:55:25 -0400

    Developing Board class -- initialized 2D array

--- script.rb
+++ script.rb
@@ -1,1 +1,15 @@
 require 'pry-byebug'
+
+class Player
+
+end
+
+class Board
+  attr_reader :board_arr
+  def initialize()
+    @board_arr = Array.new(3,Array.new(3,nil))
+  end
+end
+binding.pry
+board = Board.new
+p board
