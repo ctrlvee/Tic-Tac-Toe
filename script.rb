372d7b8bc937697460f07943a315c4168755a750
ctrlVee <me123opportunity@gmail.com>
2021-08-03 17:29:03 -0400

    Prevents user from overwriting a marker

--- script.rb
+++ script.rb
@@ -1,7 +1,5 @@
 require 'pry-byebug'
 
-
-
 class Player
   attr_reader :player_marker
 
@@ -15,15 +13,32 @@ class Player
   end
   
   def make_move(row, col)
-    $arr[row][col] = player_marker
+    
+    if check_if_marker_exists(row,col)
+      return 
+    else
+      $arr[row][col] = player_marker
+    end
+  end
+
+  def check_if_marker_exists(row, col)
+
+    if $arr[row][col] != nil
+      puts "Invalid. The other player has used that square"
+      return true
+    end
+
   end
 end
 
 class Board
-  
   def initialize()
     $arr = Array.new(3) { Array.new(3,nil) }
   end
+end
+
+class Game
+
 end
 
 binding.pry
