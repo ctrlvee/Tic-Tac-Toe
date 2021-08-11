593455fa988941a665f46ca651c9ac949d3fc183
ctrlVee <me123opportunity@gmail.com>
2021-08-10 20:07:12 -0400

    Created empty functions to check wins

--- script.rb
+++ script.rb
@@ -3,7 +3,7 @@ require 'pry-byebug'
 class Player
   attr_reader :player_marker
 
-  @@markers = %W[X O].shuffle
+  @@markers = %w[X O].shuffle
   @@player_count = 1
 
   def initialize
@@ -42,11 +42,48 @@ class Game
   def initialize
     Board.new
   end
+
+  public
+
+  def run_checks
+    check_horizontal()
+    check_diagonal()
+    check_vertical()
+  end
+
+  def check_vertical
+    p1_count = 0
+    p2_count =0
+
+    3.times do |col|
+      3.times do |row|
+        case $arr[row][col]
+        when
+
+        end
+      end
+    end
+  end
+
+  def check_horizontal
+
+  end
+
+  def check_diagonal
+
+  end
+
+  def win_count
+
+  end
 end
 
 binding.pry
+the_game = Game.new
 
 p1 = Player.new
 p2 = Player.new
 
 p1.make_move(1, 1)
+the_game.check_vertical()
+p1.make_move(1, 2)
