4e968ed3cc3a71305220a202d4ce32ae40a6f55c
ctrlVee <me123opportunity@gmail.com>
2021-09-19 20:53:13 -0400

    Improved interaction between Board class and Player class - changed  to @arr

--- script.rb
+++ script.rb
@@ -11,7 +11,6 @@ class Player
     @player_marker = @@markers.pop 
     @@player_count += 1
   end
-  
 end
 
 class Board
@@ -20,9 +19,26 @@ class Board
   def initialize
     @arr = Array.new(3) { Array.new(3, nil) }
   end
+
+  def make_move(p, row, col)
+
+    if check_if_marker_exists(row, col)
+      return 
+    else
+      @arr[row][col] = p.player_marker
+    end
+  end
+  
+  def check_if_marker_exists(row, col)
+  
+    if @arr[row][col] != nil
+      puts 'Invalid. The other player has used that square'
+      true
+    end
+  end
 end
 
-def whoWon?(p1,p2)
+def whoWon?(p1, p2)
   if p1 == 3
     return player_one
   elsif p2 == 3
@@ -67,34 +83,20 @@ def check_horizontal
     p1_count = 0
     p2_count = 0
   end
-
-  def make_move(row, col)
-    
-    if check_if_marker_exists(row,col)
-      return 
-    else
-      @arr[row][col] = player_marker
-    end
-  end
-
-  def check_if_marker_exists(row, col)
-
-    if @arr[row][col] != nil
-      puts 'Invalid. The other player has used that square'
-      true
-    end
-
-  end
-  
 end
+
+
 
 
 binding.pry
 b = Board.new
 
-@p1 = Player.new
-@p2 = Player.new
+p1 = Player.new
+p2 = Player.new
 
-b.make_move(
-#check_vertical()
-#check_horizontal()
+b.make_move(p1,1,2)
+b.make_move(p1,0,0)
+b.make_move(p2,0,0)
+puts b.arr
+#check_vertical(p1,p2)
+#check_horizontal(p1,p2)
